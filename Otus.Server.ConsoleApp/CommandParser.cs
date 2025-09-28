b0fa3b3db3ca3450a25f43528eea444fee04eed2
Сергей Шмидт <serge@MacBook-Air-Sergej.local>
2025-09-28 23:44:14 +0300

    Change split to slice, index. Add generic

--- Otus.Server.ConsoleApp/CommandParser.cs
+++ Otus.Server.ConsoleApp/CommandParser.cs
@@ -2,40 +2,48 @@ namespace Otus.Server.ConsoleApp;
 
 public static class CommandParser
 {
-    private static byte bit2 = 32;
+    private static byte space = 32;
 
-    public static CommandParts
+    public static CommandParts<ReadOnlySpan<byte>>
         Parse(ReadOnlySpan<byte> input)
     {
-        ReadOnlySpan<byte> command = ReadOnlySpan<byte>.Empty;
-        ReadOnlySpan<byte> key = ReadOnlySpan<byte>.Empty;
-        ReadOnlySpan<byte> value = ReadOnlySpan<byte>.Empty;
-
-        foreach (Range segment in input.Split(bit2))
+        int firstIndex = GetIndex(input);
+        if (firstIndex == -1)
         {
-            if (command.IsEmpty)
+            return new CommandParts<ReadOnlySpan<byte>>
             {
-                if (segment.End.Value + 1 >= input.Length)
-                {
-                    break;
-                }
-                command = input[segment];
-                continue;
-            }
-            key = input[segment];
-
-            int valueIndex = segment.End.Value + 1;
-            if (valueIndex < input.Length)
-            {
-                value = input.Slice(valueIndex);
-            }
-            break;
+                Command = ReadOnlySpan<byte>.Empty,
+                Key = ReadOnlySpan<byte>.Empty,
+                Value = ReadOnlySpan<byte>.Empty
+            };
         }
-        return new CommandParts
+        int secondIndex = GetIndex(input[(firstIndex + 1)..]);
+        if (secondIndex == -1)
         {
-            Command = command,
-            Key = key,
-            Value = value
+            return new CommandParts<ReadOnlySpan<byte>>
+            {
+                Command = input[..firstIndex],
+                Key = input[(firstIndex + 1)..],
+                Value = ReadOnlySpan<byte>.Empty
+            };
+        }
+
+        return new CommandParts<ReadOnlySpan<byte>>
+        {
+            Command = input[..firstIndex],
+            Key = input.Slice(firstIndex + 1, secondIndex),
+            Value = input[(firstIndex + secondIndex + 2)..]
         };
+
+    }
+    private static int GetIndex(ReadOnlySpan<byte> input)
+    {
+        int index = input.IndexOf(space);
+
+        if (index == -1 || input.Length == index + 1)
+        {
+            return -1;
+        }
+        return index;
     }
 }
--- Otus.Server.ConsoleApp/CommandParts.cs
+++ Otus.Server.ConsoleApp/CommandParts.cs
@@ -1,14 +1,14 @@
 namespace Otus.Server.ConsoleApp;
 
-public readonly ref struct CommandParts
+public readonly ref struct CommandParts<T> where T: allows ref struct
 {
-    public ReadOnlySpan<byte> Command { get;  init; }
-    public ReadOnlySpan<byte> Key { get;  init; }
-    public ReadOnlySpan<byte> Value { get;  init; }
+    public T Command { get;  init; }
+    public T Key { get;  init; }
+    public T Value { get;  init; }
     public void Deconstruct(
-        out ReadOnlySpan<byte> command,
-        out ReadOnlySpan<byte> key,
-        out ReadOnlySpan<byte> value)
+        out T command,
+        out T key,
+        out T value)
     {
         command = Command;
         key = Key;
