e42afc0b6a8d828234c24d3a64285cfb4767a75a
Сергей Шмидт <serge@192.168.1.4>
2025-09-28 13:47:27 +0300

    change program

--- Otus.Server.ConsoleApp/Program.cs
+++ Otus.Server.ConsoleApp/Program.cs
@@ -1,12 +1,1 @@
-﻿using System.Text;
-using Otus.Server.ConsoleApp;
-
-string input = "COMMAND";
-
-byte[] byteArray = Encoding.UTF8.GetBytes(input);
-
-var result = CommandParser.Parse(byteArray);
-
-Console.WriteLine($"command: {result.Command.Length}");
-Console.WriteLine($"key: {result.Key.Length}");
-Console.WriteLine($"value: {result.Value.Length}");
+﻿System.Console.WriteLine("Server start");
