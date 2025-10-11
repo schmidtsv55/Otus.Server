c3cfe29ae1ac4e4cc9bad2e2767437c490e52b2c
Сергей Шмидт <serge@192.168.1.4>
2025-10-11 12:22:24 +0300

    add socket server

--- Otus.Server.ConsoleApp/Program.cs
+++ Otus.Server.ConsoleApp/Program.cs
@@ -1,1 +1,10 @@
-﻿System.Console.WriteLine("Server start");
+﻿using Otus.Server.ConsoleApp;
+
+int port = 8080;
+System.Console.WriteLine($"Server start on port {port}");
+var cts = new CancellationTokenSource();
+CancellationToken cancellationToken = cts.Token;
+using var server = new TcpServer(port);
+_ = server.StartAsync(cancellationToken);
+Console.ReadLine();
+cts.Cancel();
